995bce0e9d953f1691e09a7c4b2d8b17c7de9285
Marcos <m.marcos.lop@gmail.com>
2021-12-28 20:18:06 -0300

    criado aprender js aprender hml

---
+++ aprender.js
@@ -0,0 +1,19 @@
+var calcMedia = function() {
+    return (this.nota1 + this.nota2) /2;
+}
+
+
+
+
+var turma = [
+    {
+        nome: "igor",
+        nota1:9,
+        nota2:7,
+        media: calcMedia
+    }
+]
+
+var aluno = turma[0];
+console.log(aluno);
+console.log(aluno.media());
